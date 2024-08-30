66f326dbde10b6af501eed09dceb3f6ba7babae6
siruduk <124989831+siruduk@users.noreply.github.com>
2024-08-30 18:34:11 +0900

    18:30

---
+++ GameJam/Assets/Scripts/Si/Wood.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wood : ObjectBase
+{
+    public float t;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        t++;
+        if(t>120)
+            ReleaseObject();
+        Debug.Log(t);
+    }
+}
