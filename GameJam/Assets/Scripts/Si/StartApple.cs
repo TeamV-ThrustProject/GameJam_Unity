c1b0aa4178b19fbb7838be79a8b8cdaaabe3ab4b
siruduk <124989831+siruduk@users.noreply.github.com>
2024-08-31 06:34:40 +0900

    06:35

--- GameJam/Assets/Scripts/Si/StartApple.cs
+++ GameJam/Assets/Scripts/Si/StartApple.cs
@@ -2,26 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BearRunObjectBase : ObjectBase
+public class StartApple : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-
+          
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        
     }
 
-   
     void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("End"))
+        if (other.gameObject.CompareTag("Player"))
         {
-            ReleaseObject();
+            GameManager.GameManagerInstance.Stage = 2;
+            GameManager.GameManagerInstance.Delay = 20;
         }
     }
 }
